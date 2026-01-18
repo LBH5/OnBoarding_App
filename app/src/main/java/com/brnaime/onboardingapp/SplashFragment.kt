7ec9b78bcc12bc0ddefaba92f6411458e5452046
Naime5_Br <153781554+LBH5@users.noreply.github.com>
2026-01-19 00:47:50 +0100

    feat: Add onboarding logic and home screen
    Adds a Home screen and implements the logic to show the onboarding flow only on the first app launch.
    
    - Creates `HomeFragment` and its corresponding layout.
    - Updates the navigation graph to include the `HomeFragment` and actions to navigate to it from the splash and onboarding screens.
    - In `SplashFragment`, checks `SharedPreferences` to decide whether to navigate to the onboarding flow or directly to the `HomeFragment`.
    - In `ThirdScreen` (the last onboarding screen), a "Get Started" button now navigates to the `HomeFragment` and saves a flag to `SharedPreferences` to indicate that the onboarding has been completed.

--- app/src/main/java/com/brnaime/onboardingapp/SplashFragment.kt
+++ app/src/main/java/com/brnaime/onboardingapp/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.brnaime.onboardingapp
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.brnaime.onboardingapp.onboarding.screens.ThirdScreen
 
 class SplashFragment : Fragment() {
 
@@ -19,9 +21,18 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Handler.createAsync(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+            if(onBoardingFinished()){
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            } else{
+                findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+            }
         },2500)
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun onBoardingFinished(): Boolean{
+        val sharedPref = requireActivity().getSharedPreferences(ThirdScreen.ON_BOARDING_PREF_FILENAME, MODE_PRIVATE)
+        return sharedPref.getBoolean(ThirdScreen.ON_BOARDING_PREF_KEY, false)
     }
 
 
--- app/src/main/java/com/brnaime/onboardingapp/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/brnaime/onboardingapp/onboarding/screens/ThirdScreen.kt
@@ -1,21 +1,47 @@
 package com.brnaime.onboardingapp.onboarding.screens
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
+import androidx.navigation.fragment.findNavController
 import com.brnaime.onboardingapp.R
+import com.brnaime.onboardingapp.databinding.FragmentThirdScreenBinding
 
 
 class ThirdScreen : Fragment() {
 
+    private lateinit var binding: FragmentThirdScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_third_screen, container, false)
+    ): View {
+        binding = FragmentThirdScreenBinding.inflate(inflater,container,false)
+        binding.buttonGetStarted.setOnClickListener {
+            handleGetStartedBtnClick()
+        }
+        return binding.root
+    }
+
+    private fun handleGetStartedBtnClick() {
+        findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
+        onBoardingFinished()
+    }
+
+    private fun onBoardingFinished(){
+        val sharedPref = requireActivity().getSharedPreferences(ON_BOARDING_PREF_FILENAME, MODE_PRIVATE)
+        sharedPref.edit {
+            putBoolean(ON_BOARDING_PREF_KEY, true)
+        }
+    }
+
+    companion object {
+        const val ON_BOARDING_PREF_FILENAME = "onBoarding"
+        const val ON_BOARDING_PREF_KEY = "Finished"
     }
 
 }
